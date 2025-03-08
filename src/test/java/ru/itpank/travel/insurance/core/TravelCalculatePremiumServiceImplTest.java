080eaf0d206528d5bd355a89b125f2d82268d521
engitpank <engutpunk@gmail.com>
2025-03-08 04:29:07 +0300

    STEP 013 - DONE

--- src/test/java/ru/itpank/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ src/test/java/ru/itpank/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,12 +1,33 @@
 package ru.itpank.travel.insurance.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.itpank.travel.insurance.rest.TravelCalculatePremiumRequest;
+import ru.itpank.travel.insurance.rest.TravelCalculatePremiumResponse;
+
+import java.util.Date;
 
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
-    public void deleteMe() {
+    public void calculatePremium_ValidRequest_ReturnsResponseWithCopiedFields() {
+        // Arrange
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan",
+                "Ivanov",
+                new Date(),
+                new Date()
+        );
+        TravelCalculatePremiumService travelCalculatePremiumService = new TravelCalculatePremiumServiceImpl();
 
+        // Act
+        TravelCalculatePremiumResponse actualResponse = travelCalculatePremiumService.calculatePremium(request);
+
+        // Assert
+        Assertions.assertEquals(request.getPersonFirstName(), actualResponse.getPersonFirstName());
+        Assertions.assertEquals(request.getPersonFirstName(), actualResponse.getPersonFirstName());
+        Assertions.assertEquals(request.getAgreementDateFrom(), actualResponse.getAgreementDateFrom());
+        Assertions.assertEquals(request.getAgreementDateTo(), actualResponse.getAgreementDateTo());
     }
 
 }
