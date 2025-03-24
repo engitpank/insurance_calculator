411bd478255b7750f0687ac0b4d8cc345b69501c
engitpank <engutpunk@gmail.com>
2025-03-24 23:23:39 +0300

    STEP 042 - DONE

--- src/main/java/ru/itpank/travel/insurance/dto/TravelCalculatePremiumRequest.java
+++ src/main/java/ru/itpank/travel/insurance/dto/TravelCalculatePremiumRequest.java
@@ -1,5 +1,7 @@
 package ru.itpank.travel.insurance.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.OptBoolean;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,9 @@ public class TravelCalculatePremiumRequest extends CoreResponse {
 
     private String personFirstName;
     private String personLastName;
+    @JsonFormat(pattern = "yyyy-MM-dd", lenient = OptBoolean.FALSE)
     private Date agreementDateFrom;
+    @JsonFormat(pattern = "yyyy-MM-dd", lenient = OptBoolean.FALSE)
     private Date agreementDateTo;
 
 }
